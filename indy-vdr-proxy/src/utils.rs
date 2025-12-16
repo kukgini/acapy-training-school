0746ffc32e6d639cc8ff55f563df6923269588b7
kukgini <kukgini@gmail.com>
2025-12-16 19:22:47 +0900

    Bump up indy-vdr to v0.4.4 with support Cache TTL

--- indy-vdr-proxy/src/utils.rs
+++ indy-vdr-proxy/src/utils.rs
@@ -34,41 +34,73 @@ pub fn init_pool_state_from_folder_structure(
     })?;
 
     for entry in entries {
-        let entry = entry.unwrap();
+        let entry = entry.map_err(|err| {
+            err_msg(VdrErrorKind::FileSystem, "Could not read directory entry").with_source(err)
+        })?;
+
+        let file_name = entry.file_name();
+        let file_name_str = file_name.to_str().ok_or_else(|| {
+            err_msg(VdrErrorKind::FileSystem, "Invalid UTF-8 in directory name")
+        })?;
+
         // filter hidden directories starting with "." and files
-        if !entry.file_name().to_str().unwrap().starts_with('.')
-            && entry.metadata().unwrap().is_dir()
-        {
-            let namespace = entry.path().file_name().unwrap().to_owned();
-            let sub_entries = fs::read_dir(entry.path()).unwrap();
-            for sub_entry in sub_entries {
-                let sub_entry_path = sub_entry.unwrap().path();
-                let sub_namespace = if sub_entry_path.is_dir() {
-                    sub_entry_path.file_name()
-                } else {
-                    None
-                };
-                let (ledger_prefix, genesis_txns) = match sub_namespace {
-                    Some(sub_namespace) => (
-                        format!(
-                            "{}:{}",
-                            namespace.to_str().unwrap(),
-                            sub_namespace.to_str().unwrap()
-                        ),
+        let metadata = entry.metadata().map_err(|err| {
+            err_msg(VdrErrorKind::FileSystem, "Could not read entry metadata").with_source(err)
+        })?;
+
+        if file_name_str.starts_with('.') || !metadata.is_dir() {
+            continue;
+        }
+
+        let namespace = entry
+            .path()
+            .file_name()
+            .ok_or_else(|| err_msg(VdrErrorKind::FileSystem, "Could not get directory name"))?
+            .to_owned();
+
+        let namespace_str = namespace.to_str().ok_or_else(|| {
+            err_msg(VdrErrorKind::FileSystem, "Invalid UTF-8 in namespace")
+        })?;
+
+        let sub_entries = fs::read_dir(entry.path()).map_err(|err| {
+            err_msg(
+                VdrErrorKind::FileSystem,
+                format!("Could not read subdirectory: {}", namespace_str),
+            )
+            .with_source(err)
+        })?;
+
+        for sub_entry in sub_entries {
+            let sub_entry = sub_entry.map_err(|err| {
+                err_msg(VdrErrorKind::FileSystem, "Could not read sub-entry").with_source(err)
+            })?;
+            let sub_entry_path = sub_entry.path();
+            let sub_namespace = if sub_entry_path.is_dir() {
+                sub_entry_path.file_name()
+            } else {
+                None
+            };
+            let (ledger_prefix, genesis_txns) = match sub_namespace {
+                Some(sub_namespace) => {
+                    let sub_namespace_str = sub_namespace.to_str().ok_or_else(|| {
+                        err_msg(VdrErrorKind::FileSystem, "Invalid UTF-8 in sub-namespace")
+                    })?;
+                    (
+                        format!("{}:{}", namespace_str, sub_namespace_str),
                         PoolTransactions::from_json_file(sub_entry_path.join(GENESIS_FILENAME))?,
-                    ),
-                    None => (
-                        String::from(namespace.to_str().unwrap()),
-                        PoolTransactions::from_json_file(entry.path().join(GENESIS_FILENAME))?,
-                    ),
-                };
-                let pool_state = PoolState {
-                    pool: None,
-                    last_refresh: None,
-                    transactions: genesis_txns,
-                };
-                networks.insert(ledger_prefix, pool_state);
-            }
+                    )
+                }
+                None => (
+                    String::from(namespace_str),
+                    PoolTransactions::from_json_file(entry.path().join(GENESIS_FILENAME))?,
+                ),
+            };
+            let pool_state = PoolState {
+                pool: None,
+                last_refresh: None,
+                transactions: genesis_txns,
+            };
+            networks.insert(ledger_prefix, pool_state);
         }
     }
     Ok(networks)
